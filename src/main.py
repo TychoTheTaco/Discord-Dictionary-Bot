93502db634531f0d9a0b97210286170ae647f398
Tycho Bellers <tycho.bellers999@gmail.com>
2020-09-17 00:17:58 -0700

    various changes

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import requests
 import time
 from gtts import gTTS
 import pathlib
+import io
+import json
 
 PROJECT_ROOT = pathlib.Path('../')
 
@@ -16,7 +18,59 @@ def get_token(path='token.txt'):
 
 
 def get_definition(word):
+    print('GET DEF:', word)
     return requests.get('https://owlbot.info/api/v2/dictionary/' + word.replace(' ', '%20') + '?format=json')
+
+
+async def process_word(word, message):
+    """
+
+    :param word:
+    :param message:
+    :return:
+    """
+    # Get definitions
+    response = get_definition(word)
+    print('RETURN:', response)
+    if response.status_code != 200:
+        await message.channel.send('That\'s not a word bruh')
+        return
+
+    try:
+        definitions = response.json()
+    except json.decoder.JSONDecodeError:
+        await message.channel.send('There was a problem finding that word')
+        return
+
+    # Send text chat reply
+    print('DEFINE: ', definitions)
+    reply = f'__**{word}**__\n'
+    tts_input = f'{word}, '
+    for i, definition in enumerate(definitions):
+        reply += f'**[{i + 1}]** ({definition["type"]})\n' + definition['definition'] + '\n'
+        tts_input += f'{i + 1}, {definition["type"]}, {definition["definition"]}'
+    await message.channel.send(reply)
+
+    # Create text to speech mp3
+    #tts_buffer = io.BytesIO()
+    print(tts_input)
+    tts = gTTS(tts_input)
+    urls = tts.get_urls()
+    print('URLS:', urls)
+    #tts.write_to_fp(tts_buffer)
+    #print(len(tts_buffer.getvalue()))
+    #tts.save(str(mp3_path))
+
+    # Join voice channel
+    voice_channel = message.author.voice.channel
+    if voice_channel is not None:
+        voice_client = await voice_channel.connect()
+
+        for url in urls:
+            voice_client.play(discord.FFmpegPCMAudio(url, executable=str(pathlib.Path(PROJECT_ROOT, 'ffmpeg-20200831-4a11a6f-win64-static/bin/ffmpeg.exe'))))
+            while voice_client.is_playing():
+                time.sleep(1)
+        await voice_client.disconnect()
 
 
 class Client(discord.Client):
@@ -46,36 +100,7 @@ class Client(discord.Client):
             word = ' '.join(command[1:])
             print('word:', word)
 
-            # Replace spaces
-            response = get_definition(word)
-            if response.status_code is not 200:
-                await message.channel.send('That\'s not a word bruh')
-            else:
-                definitions = response.json()
-
-                # Send text chat reply
-                print('DEFINE: ', definitions)
-                reply = f'__**{word}**__\n'
-                tts_input = f'{word}.'
-                for i, definition in enumerate(definitions):
-                    reply += f'**[{i + 1}]** ({definition["type"]})\n' + definition['definition'] + '\n'
-                    tts_input += f'{i + 1}. {definition["type"]}. {definition["definition"]}'
-                await message.channel.send(reply)
-
-                # Create text to speech mp3
-                mp3_path = pathlib.Path(Client.TTS_CACHE, 'reply.mp3')
-                tts = gTTS(tts_input)
-                tts.save(str(mp3_path))
-
-                # Join voice channel
-                connected = message.author.voice
-                if connected:
-                    voice_channel = await connected.channel.connect()
-
-                    voice_channel.play(discord.FFmpegPCMAudio(str(mp3_path), executable=str(pathlib.Path(PROJECT_ROOT, 'ffmpeg-20200831-4a11a6f-win64-static/bin/ffmpeg.exe'))))
-                    while voice_channel.is_playing():
-                        time.sleep(1)
-                    await voice_channel.disconnect()
+            await process_word(word, message)
 
 
 client = Client()
