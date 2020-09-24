5e290af696d393ad834d4929f411087f7d4f170b
Tycho Bellers <tycho.bellers999@gmail.com>
2020-09-23 21:40:27 -0700

    bug fix

--- src/main.py
+++ src/main.py
@@ -224,7 +224,8 @@ class DictionaryBotClient(discord.Client):
 
                     # Add word to the queue
                     print('COMMAND:', command_input)
-                    text_to_speech = len(command_input[0]) == 2 and command_input[0] == 'v'
+                    text_to_speech = len(command_input[0]) == 2 and command_input[0][1] == 'v'
+                    print(command_input[0])
                     self._definition_response_manager.add(word, message, command_input[0] == 'b', text_to_speech=text_to_speech)
                 elif command._name == 'stop':
                     # Clear word queue
