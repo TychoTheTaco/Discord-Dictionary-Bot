d6a3f79e672ac36e99ff8fb907a734394a6de8aa
Tycho Bellers <tycho.bellers999@gmail.com>
2020-09-26 23:32:29 -0700

    split message into chunks due to a bug with 4mb response limit

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
 
-def split(string, split_size):
-    pass
+def split(message, split_size):
+    messages = []
+    while len(message) > 0:
+
+        # Find closest space before 'split_size' limit
+        if len(message) > split_size:
+            space_index = split_size
+            while message[space_index] != ' ':
+                space_index -= 1
+        else:
+            space_index = len(message)
+
+        # Add chunk to message list
+        m = message[:space_index]
+        messages.append(m)
+
+        # Remove chunk from message
+        message = message[space_index:]
+
+    return messages
 
 if __name__ == '__main__':
-    text = 'This is a really long message that needs to be split on spaces so that the text to speech synthesizer can process it correctly. It needs to be split into chunks preferably by sentences to there arent any weird pauses.'
+    text = 'This is a really long message that needs to somehow be split on spaces so that the text to speech synthesizer can process it correctly. It needs to be split into chunks preferably by sentences to there arent any weird pauses.'
     result = split(text, 40)
-    print(result)
+    for m in result:
+        print(len(m), m)
