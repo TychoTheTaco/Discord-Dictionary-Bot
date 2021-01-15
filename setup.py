8cd1fdc9f60d56267b0a8289cc3781cb92566f11
Tycho Bellers <tycho.bellers999@gmail.com>
2021-01-14 17:45:42 -0800

    update version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='discord-dictionary-bot', version='0.0.1', packages=find_packages())
+setup(name='discord-dictionary-bot', version='1.0.0', packages=find_packages())
